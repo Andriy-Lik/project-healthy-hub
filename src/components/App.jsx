42b610997765670a8ecfb5f8d4ff58942750bbb8
Renita <ira3624@icloud.com>
2023-09-16 15:46:10 +0300

    ups

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
-import { RecommendedFood } from "pages/RecommendedFoodPage/RecommendedFoodPage";
 import  Header  from "./Header/Header";
 
 export const App = () => {
   return (
     <>
     <Header title={ "Hello World"} />
-    <RecommendedFood/>
     </>
   );
 };
