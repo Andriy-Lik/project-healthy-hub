65a9359081481d9ab421f20c2010f8e586b4b66e
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-09-16 23:07:25 +0300

    up

--- src/pages/WellcomPage/WellcomPage.js
+++ src/pages/WellcomPage/WellcomPage.js
@@ -5,18 +5,18 @@ const WelcomePage = () => {
   return (
     <div>
       <div className={styles.backgroundContainer}>
-        <img src={welcomeLogo} alt='Reg Logo'/>
+        <img src={welcomeLogo} className={styles.welcomeLogo} alt='Reg Logo'/>
         <div className={styles.perksContainer}>
-          <p className={styles.welcomeHeadline}>Set goals and achieve them</p>
+          <h1 className={styles.welcomeHeadline}>Set goals and achieve them</h1>
           <p className={styles.welcomeText}>The service will help you set goals and follow them.</p>
           <button className={styles.signInActivated}>Sign in</button>
           <button className={styles.signUpInactive}>Sign up</button>
 
           <ul className={styles.welcomePagePerksList}>
-            <li>Set goals</li>
-            <li>Watch your calories</li>
-            <li>Keep track of your water intake</li>
-            <li>Control your weight</li>
+            <li className={styles.perks}>Set goals</li>
+            <li className={styles.perks}>Watch your calories</li>
+            <li className={styles.perks}>Keep track of your water intake</li>
+            <li className={styles.perks}>Control your weight</li>
           </ul>
         </div>
       </div>
