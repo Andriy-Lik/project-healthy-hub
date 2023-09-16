aa18e34742aa817d0af5860b622c584265fd0e0c
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-09-16 22:16:04 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header/Header';
 import Dashboard from 'pages/DashboardPage/DashboardPage';
+import WelcomePage from 'pages/WellcomPage/WellcomPage';
 
 export const App = () => {
   return (
     <div>
       <Header title={'Hello World'} />
+      <WelcomePage />
       <Dashboard />
     </div>
   );
--- src/pages/WellcomPage/WellcomPage.js
+++ src/pages/WellcomPage/WellcomPage.js
@@ -1,35 +1,26 @@
-import { Link } from "react-router-dom";
+import styles from './WellcomPage.module.css';
+import welcomeLogo from '../../images/WelcomeLogo.png';
 
 const WelcomePage = () => {
-
   return (
     <div>
-        <header>
-            <h2>HealthyHub</h2>
-            <Link>Sign in</Link>
-            /
-            <Link>Sign up</Link>
-            {/* <img /> */}
-        </header>
+      <div className={styles.backgroundContainer}>
+        <img src={welcomeLogo} alt='Reg Logo'/>
+        <div className={styles.perksContainer}>
+          <p className={styles.welcomeHeadline}>Set goals and achieve them</p>
+          <p className={styles.welcomeText}>The service will help you set goals and follow them.</p>
+          <button className={styles.signInActivated}>Sign in</button>
+          <button className={styles.signUpInactive}>Sign up</button>
 
-        <div>
-            {/* <img /> */}
-            <div>
-                <p>Set goals and achieve them</p>
-                <p>The service will help you set goals
-and follow them.</p>
-                <button>Sign in</button>
-                <button>Sign up</button>
-
-                <ul>
-                    <li>Set goals</li>
-                    <li>Watch your calories</li>
-                    <li>Keep track of your water intake</li>
-                    <li>Control your weight</li>
-                </ul>
-            </div>
+          <ul className={styles.welcomePagePerksList}>
+            <li>Set goals</li>
+            <li>Watch your calories</li>
+            <li>Keep track of your water intake</li>
+            <li>Control your weight</li>
+          </ul>
         </div>
+      </div>
     </div>
-  )
+  );
 };
 export default WelcomePage;
