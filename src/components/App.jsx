2b422aa886f6002290396fd36304007d087e8ba1
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-09-17 18:06:28 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header/Header';
 import Dashboard from 'pages/DashboardPage/DashboardPage';
+import WelcomePage from 'pages/WelcomePage/WelсomePage';
 
 export const App = () => {
   return (
     <div>
       <Header title={'Hello World'} />
+      <WelcomePage />
       <Dashboard />
     </div>
   );
--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -1,36 +1,38 @@
-.backgroundContainer {
+import styled from "styled-components";
+
+export const BackgroundContainer = styled.div `
     background-color: #050505;
     display: flex;
-}
+`;
 
-.welcomeLogo {
+export const WelcomeLogo = styled.img `
     margin-left: 150px;
     margin-top: 40px;
-}
+`;
 
-.perksContainer {
+export const PerksContainer = styled.div ` 
     max-width: 444px;
     margin-top: 120px;
     margin-left: 104px;
-}
+`;
 
-.welcomeHeadline {
+export const WelcomeHeadline = styled.h1 ` 
     color: #FFFFFF;
     font-size: 30px;
     font-weight: 500;
     line-height: 36px;
     font-family: Poppins;
-}
+`;
 
-.welcomeText {
+export const WelcomeText = styled.p ` 
     color: #B6B6B6;
     font-weight: 400;
     font-size: 22px;
     margin-top: 16px;
     line-height: 32px;
-}
+`;
 
-.signInActivated {
+export const SignInActivated = styled.button ` 
     background-color: #E3FFA8;
     margin-top: 24px;
     border-radius: 12px;
@@ -38,27 +40,27 @@
     padding: 8px;
     font-size: 14px;
     font-family: Poppins;
-}
+`;
 
-.signUpInactive {
+export const SignUpInactive = styled.button ` 
     background-color: #050505;
     border: none;
     font-size: 14px;
     color: #FFFFFF;
     margin-left: 16px;
-}
+`;
 
-.welcomePagePerksList {
+export const WelcomePagePerksList = styled.ul ` 
     margin-top: 80px;
-}
+`
 
-.perks {
+export const Perks = styled.li ` 
     color: #FFFFFF;
     margin-bottom: 22px;
-}
 
-.perks::before {
-    content: '\2022';
-    color: #E3FFA8;
-    margin-right: 5px;
-}
+    ::before {
+        content: '\2022';
+        color: #E3FFA8;
+        margin-right: 5px;
+    }
+`
