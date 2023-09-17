0a7b6b5b48f1e0792146133e62f62d28e08b6832
Andriy-Lik <alan321987@gmail.com>
2023-09-17 21:44:32 +0300

    добавив компонент хедер в SharedLayout.js

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,9 +1,11 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../Header";
 
 const SharedLayout = () => {
     return (
         <div>
+            <Header />
             <Suspense fallback={null}>
                 <Outlet />
             </Suspense>
