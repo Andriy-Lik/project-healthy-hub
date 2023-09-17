a56b6ac2035593c1ac274a98bca03fd0f3d50f26
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-09-17 18:09:15 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import Header from './Header/Header';
 import Dashboard from 'pages/DashboardPage/DashboardPage';
-import WelcomePage from 'pages/WelcomePage/WelсomePage';
 
 export const App = () => {
   return (
     <div>
       <Header title={'Hello World'} />
-      <WelcomePage />
       <Dashboard />
     </div>
   );
