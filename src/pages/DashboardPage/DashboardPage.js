34d1cf4d90e786a662655892ef05673466e59f48
Olia Rosiak <olia.rosiak@gmail.com>
2023-09-18 16:16:18 +0300

    fix dashboard markup

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,70 +1,76 @@
 import {
+  DashboardSection,
   DashboardContainer,
   HeaderBlock,
-  MainHeader,
+  MainHeaderBlock,
+  ArrowReturn,
+  Header,
+  ArrowDown,
   SecondHeader,
-  ChartsBlock,
-  ChartsList,
-  ChartsContainer,
-  ChartsHeader,
+  TitleContainer,
   ChartsTitle,
   ChartsSubtitle,
-  Charts,
-  ScaleBlock,
+  ChartsCaption,
+  LineChartBlock,
+  ChartGrid,
+  Chart,
+  ScaleChartBlock,
   Scale,
 } from './DashboardPage.styled';
 
+import arrowDown from '../../images/icons/arrow-down.svg';
+import arrowRight from '../../images/icons/arrow-right.svg';
 import LineChart from 'components/Charts/LineChart';
 // import ScaleChart from 'components/Charts/ScaleChart';
 
-const Dashboard = () => {
+const DashboardPage = () => {
   const calories = 1700;
   const water = 1600;
   const weight = 68;
   return (
-    <DashboardContainer>
-      <HeaderBlock>
-        <MainHeader>Last month</MainHeader>
-        <SecondHeader>November</SecondHeader>
-      </HeaderBlock>
-      <ChartsBlock>
-        <ChartsList>
-          <ChartsContainer>
-            <ChartsHeader>Calories</ChartsHeader>
-            <ChartsContainer>
-              <ChartsTitle>Average value:</ChartsTitle>
-              <ChartsSubtitle>{calories} kg</ChartsSubtitle>
-            </ChartsContainer>
-          </ChartsContainer>
-          <Charts>
-            <LineChart />
-          </Charts>
-        </ChartsList>
-        <ChartsList>
-          <ChartsContainer>
-            <ChartsHeader>Water</ChartsHeader>
-            <ChartsContainer>
-              <ChartsTitle>Average value:</ChartsTitle>
-              <ChartsSubtitle>{water} ml</ChartsSubtitle>
-            </ChartsContainer>
-          </ChartsContainer>
-          <Charts>
-            <LineChart />
-          </Charts>
-        </ChartsList>
-      </ChartsBlock>
-      <ScaleBlock>
-        <ChartsContainer>
-          <ChartsHeader>Weight</ChartsHeader>
-          <ChartsContainer>
-            <ChartsTitle>Average value:</ChartsTitle>
-            <ChartsSubtitle>{weight} kg</ChartsSubtitle>
-          </ChartsContainer>
-        </ChartsContainer>
-        <Scale>{/* <ScaleChart /> */}</Scale>
-      </ScaleBlock>
-    </DashboardContainer>
+    <DashboardSection>
+      <DashboardContainer>
+        <HeaderBlock>
+          <MainHeaderBlock>
+            <ArrowReturn src={arrowRight} alt="arrow right" />
+            <Header>Last month</Header>
+            <ArrowDown src={arrowDown} alt="arrow down" />
+          </MainHeaderBlock>
+          <SecondHeader>November</SecondHeader>
+        </HeaderBlock>
+        <LineChartBlock>
+          <ChartGrid>
+            <TitleContainer>
+              <ChartsTitle>Calories</ChartsTitle>
+              <ChartsSubtitle>Average value:</ChartsSubtitle>
+              <ChartsCaption>{calories} kg</ChartsCaption>
+            </TitleContainer>
+            <Chart>
+              <LineChart />
+            </Chart>
+          </ChartGrid>
+          <ChartGrid>
+            <TitleContainer>
+              <ChartsTitle>Water</ChartsTitle>
+              <ChartsSubtitle>Average value:</ChartsSubtitle>
+              <ChartsCaption>{water} ml</ChartsCaption>
+            </TitleContainer>
+            <Chart>
+              <LineChart />
+            </Chart>
+          </ChartGrid>
+        </LineChartBlock>
+        <ScaleChartBlock>
+          <TitleContainer>
+            <ChartsTitle>Weight</ChartsTitle>
+            <ChartsSubtitle>Average value:</ChartsSubtitle>
+            <ChartsCaption>{weight} kg</ChartsCaption>
+          </TitleContainer>
+          <Scale>jgjgjggjgjgjgjgjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</Scale>
+        </ScaleChartBlock>
+      </DashboardContainer>
+    </DashboardSection>
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
