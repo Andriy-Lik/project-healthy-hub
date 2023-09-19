13108767407527f582071ea8643dda8af012b96e
ViktoriaDufynets <52819403+ViktoriaDufynets@users.noreply.github.com>
2023-09-19 13:06:55 +0300

    change app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
-import Header from './Header/Header';
-import Dashboard from 'pages/DashboardPage/DashboardPage';
-import Diary from 'pages/DiaryPage/DiaryPage';
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import SharedLayout from './SharedLayout';
+
+const WelcomePage = lazy(() => import('../pages/WelcomePage'));
+const SignUpPage = lazy(() => import('../pages/SignUpPage'));
+const SignInPage = lazy(() => import('../pages/SignInPage'));
+const ForgotPasswordPage = lazy(() => import('../pages/ForgotPasswordPage'));
 
 export const App = () => {
   return (
-    <div>
-      <Header title={'Hello World'} />
-      <Dashboard />
-      <Diary />
-    </div>
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/signin' element={<SignInPage />} />
+        <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+      </Route>
+    </Routes>
   );
 };
