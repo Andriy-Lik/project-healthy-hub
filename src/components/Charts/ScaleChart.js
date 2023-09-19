5f4a42ff592f23b4131abb4cac611eba7cb810f1
Olia Rosiak <olia.rosiak@gmail.com>
2023-09-19 15:03:42 +0300

    upd ScaleChart
    upd ScaleChart.js (for month) + add styles, upd import in dashboardpage

--- src/components/Charts/ScaleChart.js
+++ src/components/Charts/ScaleChart.js
@@ -1,222 +1,53 @@
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from 'chart.js';
-// import { Line } from 'react-chartjs-2';
+import { List, Item, WeightTitle, DataTitle } from './ScaleChart.styled';
 
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// export const options = {
-//   responsive: true,
-//   indexAxis: 'x',
-//   maintainAspectRatio: true,
-//   scales: {
-//     y: {
-//       display: true,
-//       beginAtZero: true,
-//       lineWidth: 0,
-//       //   max: 1,
-//       //   ticks: {
-//       //     stepSize: 100,
-//       //   },
-//     },
-//   },
-
-//   plugins: {
-//     legend: {
-//       display: false,
-//     },
-//   },
-// };
-
-// const labels = [
-//   '1',
-//   '2',
-//   '3',
-//   '4',
-//   '5',
-//   '6',
-//   '7',
-//   '8',
-//   '9',
-//   '10',
-//   '11',
-//   '12',
-//   '13',
-//   '14',
-//   '15',
-//   '16',
-//   '17',
-//   '18',
-//   '19',
-//   '20',
-//   '21',
-//   '22',
-//   '23',
-//   '24',
-//   '25',
-//   '26',
-//   '27',
-//   '28',
-//   '29',
-//   '30',
-// ];
-
-// const weightMonth = [
-//   65, 68, 70, 70, 71, 64, 63, 64, 65, 64, 63, 64, 65, 64, 63, 64, 65, 64, 63,
-//   64, 65, 64, 63, 64, 65, 65, 64, 63, 64, 65,
-// ];
-
-// const data1 = {
-//   labels,
-//   datasets: [
-//     {
-//       data: [
-//         { x: 1, y: 0 },
-//         { x: 2, y: 0 },
-//         { x: 3, y: 0 },
-//       ],
-//       fill: false,
-//       showLine: true,
-//       borderColor: '#E3FFA8',
-//       borderWidth: 1,
-//       tension: 0.4,
-//       pointRadius: 0,
-//     },
-//   ],
-// };
-// const data2 = {
-//   labels: weightMonth,
-//   datasets: [
-//     {
-//       borderColor: '#E3FFA8',
-//       borderWidth: 1,
-//       pointRadius: 0,
-//     },
-//   ],
-// };
-
-import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-} from 'chart.js';
-
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
-
-const labels = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-  '17',
-  '18',
-  '19',
-  '20',
-  '21',
-  '22',
-  '23',
-  '24',
-  '25',
-  '26',
-  '27',
-  '28',
-  '29',
-  '30',
+const beckendArray = [
+  { data: '01.02.2023', weight: '65' },
+  { data: '02.02.2023', weight: '69' },
+  { data: '03.02.2023', weight: '65' },
+  { data: '04.02.2023', weight: '70' },
+  { data: '05.02.2023', weight: '69' },
+  { data: '06.02.2023', weight: '69' },
+  { data: '07.02.2023', weight: '71.5' },
+  { data: '08.02.2023', weight: '67' },
+  { data: '09.02.2023', weight: '65' },
+  { data: '10.02.2023', weight: '65' },
+  { data: '11.02.2023', weight: '68' },
+  { data: '12.02.2023', weight: '65' },
+  { data: '13.02.2023', weight: '65' },
+  { data: '14.02.2023', weight: '65' },
+  { data: '15.02.2023', weight: '65' },
+  { data: '16.02.2023', weight: '70.5' },
+  { data: '17.02.2023', weight: '65' },
+  { data: '03.02.2023', weight: '65' },
+  { data: '18.02.2023', weight: '65' },
+  { data: '19.02.2023', weight: '65' },
+  { data: '20.02.2023', weight: '65' },
+  { data: '21.02.2023', weight: '65' },
+  { data: '22.02.2023', weight: '65' },
+  { data: '23.02.2023', weight: '70.5' },
+  { data: '24.02.2023', weight: '69' },
+  { data: '25.02.2023', weight: '68' },
+  { data: '26.02.2023', weight: '69' },
+  { data: '27.02.2023', weight: '70.5' },
+  { data: '28.02.2023', weight: '69' },
+  { data: '29.02.2023', weight: '67.5' },
+  { data: '30.02.2023', weight: '66' },
+  { data: '31.02.2023', weight: '65' },
 ];
-
-const data = [
-  65, 68, 70, 70, 71, 64, 63, 64, 65, 64, 63, 64, 65, 64, 63, 64, 65, 64, 63,
-  64, 65, 64, 63, 64, 65, 65, 64, 63, 64, 65,
-];
-
-const datasets = [
-  {
-    data: data,
-    fill: false,
-    showLine: true,
-    borderColor: '#E3FFA8',
-    borderWidth: 1,
-    tension: 0.4,
-    pointRadius: 0,
-  },
-];
-const dataOne = {
-  labels,
-  datasets,
-};
-
-export const options = {
-  responsive: true,
-  scales: {
-    y: {
-      display: false,
-      min: 50,
-      max: 80,
-    },
-  },
-  plugins: {
-    legend: {
-      display: false,
-    },
-    title: {
-      display: false,
-    },
-  },
-};
-
-// const average = Math.round(_.mean(data));
-
-// const WeightChart = () => {
-//   return (
-//     <div className={css.caloriesChart}>
-//       <div className={css.dashboardTitle}>
-//         <p className={css.chartTitle}>Weight</p>
-//         <p className={css.chartSubtitle}>Average value: {average} kg</p>
-//       </div>
-//       <div className={css.chart}>
-//         <Line options={options} data={dataOne} />
-//       </div>
-//     </div>
-//   );
-// };
 
 const ScaleChart = () => {
   return (
-    <div>
-      <Line options={options} data={dataOne} />
-      {/* <Line options={options} data={data2} />; */}
-      {/* <Line options={options} data={data1} />; */}
-    </div>
+    <List>
+      {beckendArray.map(({ data, weight }) => {
+        const number = data.split('.').slice(0, 1);
+        return (
+          <Item key={data}>
+            <WeightTitle>{weight}</WeightTitle>
+            <DataTitle>{number}</DataTitle>
+          </Item>
+        );
+      })}
+    </List>
   );
 };
 
