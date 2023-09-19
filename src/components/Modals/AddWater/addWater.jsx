c97e001fdc823f97ae528c4addda80064a1fe95b
Renita <ira3624@icloud.com>
2023-09-19 17:24:16 +0300

    addWater

--- src/components/Modals/AddWater/addWater.jsx
+++ src/components/Modals/AddWater/addWater.jsx
@@ -1,52 +1,59 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
-// import { useState } from "react";
-import * as yup from 'yup'
-import {Backdrop, Modal, Title, Label, Button, ButtonTwo} from './addWater.styled'
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
+import {
+  Backdrop,
+  Modal,
+  Title,
+  Label,
+  Button,
+  ButtonTwo,
+  Input,
+  ErrorMes,
+} from './addWater.styled';
 
- const schema = yup.object({
-    water: yup.number().required().positive().integer(),
-  });
+const schema = yup.object({
+  water: yup.number().required().positive().integer(),
+});
 
-  const initialValues= {
-    water: ''
-   }
-  
+const initialValues = {
+  water: '',
+};
+
 export function AddWater() {
-//     const [isModalOpen, setModalOpen] = useState(false);
+  //     const [isModalOpen, setModalOpen] = useState(false);
 
-//   const openModal = () => {
-//     setModalOpen(true);
-//   };
+  //   const openModal = () => {
+  //     setModalOpen(true);
+  //   };
 
-//   const closeModal = () => {
-//     setModalOpen(false);
-//   };
+  //   const closeModal = () => {
+  //     setModalOpen(false);
+  //   };
 
- const handleSubmit = (values, {resetForm}) => {
+  const handleSubmit = (values, { resetForm }) => {
     console.log(values);
     resetForm();
- }
+  };
 
-    return (
-        <Backdrop>
-            <Modal>
-          <Title>Add water intake</Title>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={handleSubmit}
-            validationSchema={schema}
-          >
-            <Form autoComplete="off">
-                <Label htmlFor="water">
-                How much water</Label>
-              <Field name="water" type="text"/>
-              <ErrorMessage name='water' component='div'/>
-              
-              <Button type="submit">Confirm</Button>
-              <ButtonTwo type="button">Cancel</ButtonTwo>
-            </Form>
-          </Formik>
-          </Modal>
-        </Backdrop>
-      );
+  return (
+    <Backdrop>
+      <Modal>
+        <Title>Add water intake</Title>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={schema}
+        >
+          <Form autoComplete="off">
+            <Label htmlFor="water">How much water</Label>
+            <Input name="water" type="text" />
+            <ErrorMes name="water" component="div" />
+
+            <Button type="submit">Confirm</Button>
+            <ButtonTwo type="button">Cancel</ButtonTwo>
+          </Form>
+        </Formik>
+      </Modal>
+    </Backdrop>
+  );
 }
