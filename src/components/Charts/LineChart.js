569e243ed459726e0ed9715744988f819bc8ddcf
Olia Rosiak <olia.rosiak@gmail.com>
2023-09-20 20:16:39 +0300

    upd LineChart (draft)

--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -20,6 +22,54 @@ ChartJS.register(
   Legend
 );
 
+const beckendArrayMonth = [
+  { data: '1', water: '2100', calories: '1550' },
+  { data: '2', water: '1500', calories: '1850' },
+  { data: '3', water: '2100', calories: '1550' },
+  { data: '4', water: '1700', calories: '1850' },
+  { data: '5', water: '1500', calories: '1550' },
+  { data: '6', water: '2100', calories: '1500' },
+  { data: '7', water: '1700', calories: '1850' },
+  { data: '8', water: '1950', calories: '1550' },
+  { data: '9', water: '1950', calories: '1850' },
+  { data: '10', water: '2100', calories: '1850' },
+  { data: '11', water: '1700', calories: '1550' },
+  { data: '12', water: '1700', calories: '1850' },
+  { data: '13', water: '1950', calories: '1650' },
+  { data: '14', water: '2100', calories: '1650' },
+  { data: '15', water: '1700', calories: '1850' },
+  { data: '16', water: '1650', calories: '1650' },
+  { data: '17', water: '1700', calories: '1850' },
+  { data: '18', water: '1500', calories: '1850' },
+  { data: '19', water: '1650', calories: '1850' },
+  { data: '20', water: '2100', calories: '1550' },
+  { data: '21', water: '1700', calories: '1850' },
+  { data: '22', water: '1700', calories: '1850' },
+  { data: '23', water: '1700', calories: '1850' },
+  { data: '24', water: '2100', calories: '1850' },
+  { data: '25', water: '1700', calories: '1550' },
+  { data: '26', water: '1700', calories: '1600' },
+  { data: '27', water: '1700', calories: '1850' },
+  { data: '28', water: '2100', calories: '1600' },
+  { data: '29', water: '1900', calories: '1850' },
+  { data: '30', water: '1900', calories: '1600' },
+];
+
+const beckendArrayYear = [
+  { data: 'January', water: '1700', calories: '1850' },
+  { data: 'February', water: '1800', calories: '1600' },
+  { data: 'March', water: '1950', calories: '1650' },
+  { data: 'April', water: '1700', calories: '1950' },
+  { data: 'May', water: '1850', calories: '2000' },
+  { data: 'June', water: '1700', calories: '1850' },
+  { data: 'July', water: '1950', calories: '1750' },
+  { data: 'August', water: '2000', calories: '1700' },
+  { data: 'September', water: '1700', calories: '1900' },
+  { data: 'October', water: '2100', calories: '1550' },
+  { data: 'November', water: '2150', calories: '1600' },
+  { data: 'December', water: '1900', calories: '1650' },
+];
+
 export const options = {
   responsive: true,
   scales: {
@@ -40,7 +90,6 @@ export const options = {
         display: true,
         color: '#292928',
       },
-      // offset: 'top',
     },
   },
   plugins: {
@@ -50,57 +99,66 @@ export const options = {
   },
 };
 
-const labels = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-  '17',
-  '18',
-  '19',
-  '20',
-  '21',
-  '22',
-  '23',
-  '24',
-  '25',
-  '26',
-  '27',
-  '28',
-  '29',
-  '30',
-];
+const LineChart = ({ dataFormat, type }) => {
+  const [waterData, setWaterData] = useState([]);
+  const [calData, setCalData] = useState([]);
+  const [time, setTime] = useState([]);
 
-const data = {
-  labels,
-  datasets: [
-    {
-      data: labels.map(() => {
-        const randomNum = 1500 + Math.random() * 100;
-        return Math.round(randomNum / 100) * 100;
-      }),
-      borderColor: '#E3FFA8',
-      borderWidth: 1,
-      pointRadius: 0,
-      tension: 0.5,
-    },
-  ],
-};
+  useEffect(() => {
+    if (dataFormat) {
+      const yearTime = beckendArrayYear.map(time => time.data);
+      const yearInfoWater = beckendArrayYear.map(info => info.water);
+      const yearInfoCal = beckendArrayYear.map(info => info.calories);
 
-const LineChart = () => {
-  return <Line options={options} data={data} />;
+      setWaterData(yearInfoWater);
+      setCalData(yearInfoCal);
+      setTime(yearTime);
+    }
+    if (!dataFormat) {
+      const monthTime = beckendArrayMonth.map(time => time.data);
+      const monthInfoWater = beckendArrayMonth.map(info => info.water);
+      const monthInfoCal = beckendArrayMonth.map(info => info.calories);
+
+      setWaterData(monthInfoWater);
+      setCalData(monthInfoCal);
+      setTime(monthTime);
+    }
+  }, [dataFormat, type]);
+
+  const water = {
+    labels: time,
+    datasets: [
+      {
+        data: waterData,
+        borderColor: '#E3FFA8',
+        borderWidth: 1,
+        pointRadius: 0,
+        tension: 0.5,
+      },
+    ],
+  };
+  const cal = {
+    labels: time,
+    datasets: [
+      {
+        data: calData,
+        borderColor: '#E3FFA8',
+        borderWidth: 1,
+        pointRadius: 0,
+        tension: 0.5,
+      },
+    ],
+  };
+
+  return (
+    <>
+      {type === 'water' ? (
+        <Line options={options} data={water} />
+      ) : (
+        <Line options={options} data={cal} />
+      )}
+    </>
+  );
 };
 
 export default LineChart;
