622a2b74010826621ae00b3148ac5f35cfd749cf
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-09-20 12:00:44 +0200

    SettingPage(without radio and btn at the bottom)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,17 @@ const WelcomePage = lazy(() => import('../pages/WelcomePage'));
 const SignUpPage = lazy(() => import('../pages/SignUpPage'));
 const SignInPage = lazy(() => import('../pages/SignInPage'));
 const ForgotPasswordPage = lazy(() => import('../pages/ForgotPasswordPage'));
+const SettingsPage = lazy(() => import('../pages/SettingsPage'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<SharedLayout />}>
+      <Route path="/" element={<SharedLayout />}>
         <Route index element={<WelcomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/signin' element={<SignInPage />} />
-        <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/settings-page" element={<SettingsPage />} />
       </Route>
     </Routes>
   );
