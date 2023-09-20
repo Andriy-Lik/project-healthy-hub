b7f21e489a1816e68265bf6ce3f022282309bb9d
Andriy-Lik <alan321987@gmail.com>
2023-09-20 22:18:42 +0300

    LocalStorage
    Добавлена возможность сохранения токена в LocalStorage. Изменен фавикон сайта

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import { authReducer } from "./Auth/authSlice";
+
+const persistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+export const persistor = persistStore(store);
