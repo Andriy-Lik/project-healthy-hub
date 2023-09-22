eaf247f125e6da5571f03706021ecab9ccb361b7
Andriy-Lik <alan321987@gmail.com>
2023-09-22 10:37:20 +0300

    видалив закоментований код

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,6 @@ const DiaryPage = lazy(() => import('../pages/DiaryPage'));
 const RecommendedFoodPage = lazy(() => import('../pages/RecommendedFoodPage'));
 const SettingsPage = lazy(() => import('../pages/SettingsPage'));
 
-// import MainPage from "pages/MainPage";
 export const App = () => {
   return (
     <Routes>
@@ -39,6 +38,5 @@ export const App = () => {
         <Route path='/settings' element={<PrivateRoute redirectTo='/signin' component={<SettingsPage />} />} />
       </Route>
     </Routes>
-    // <MainPage/>
   ); 
 };
