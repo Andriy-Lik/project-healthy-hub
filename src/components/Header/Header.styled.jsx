2a711525bc80b45e34be48eb6dea2b1d51954fce
Vania Bogdan <bogdan.vania@gmail.com>
2023-09-23 23:15:43 +0300

    header updated

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
@@ -70,7 +71,7 @@ export const Info = styled.div`
 
 export const InfoOptions = styled.div`
   display: flex;
-`
+`;
 
 export const InfoBlock = styled.div`
   background-color: inherit;
@@ -82,14 +83,14 @@ export const InfoBlock = styled.div`
   }
 
   @media screen and (${props => props.theme.mq.tablet}) {
-  display: flex;
+    display: flex;
     &:nth-child(1) {
       margin-right: 40px;
     }
   }
 
   @media screen and (${props => props.theme.mq.desktop}) {
-  display: flex;
+    display: flex;
     &:nth-child(1) {
       margin-right: 80px;
     }
@@ -111,13 +112,13 @@ export const TextContainer = styled.div`
   flex-direction: column;
   justify-content: space-around;
   padding: 3px 0 3px 0;
-`
+`;
 
 export const InfoBlockName = styled.p`
   color: ${p => p.theme.colors.white};
   font-size: ${p => p.theme.fontSizes.xs};
   font-weight: ${p => p.theme.fontWeights.semiBold};
-`
+`;
 
 export const InfoBlockText = styled.p`
   color: ${p => p.theme.colors.white};
@@ -125,12 +126,12 @@ export const InfoBlockText = styled.p`
   font-weight: ${p => p.theme.fontWeights.medium};
   display: flex;
   align-items: center;
-`
+`;
 
 export const ArrowSvg = styled.img`
   width: 14px;
   margin-left: 12px;
-`
+`;
 
 export const WeightKg = styled.p`
   color: ${p => p.theme.colors.gray};
@@ -138,11 +139,11 @@ export const WeightKg = styled.p`
   font-weight: ${p => p.theme.fontWeights.regular};
   margin-left: 4px;
   margin-right: 6px;
-`
+`;
 
 export const EditSvg = styled.img`
   width: 16px;
-`
+`;
 
 export const UserBlock = styled.div`
   color: ${p => p.theme.colors.white};
@@ -183,4 +184,12 @@ export const Unauthorized = styled.div`
   @media screen and (${props => props.theme.mq.desktop}) {
     margin-right: 34px;
   }
+`;
+
+export const DefaultLink = styled(Link)`
+  color: ${p => p.theme.colors.white};
+`;
+
+export const ActiveLink = styled(Link)`
+  color: ${p => p.theme.colors.violet};
 `;
