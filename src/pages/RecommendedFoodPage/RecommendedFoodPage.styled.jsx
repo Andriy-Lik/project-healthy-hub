2188feb77303c76868828f07eb08731ec7b76e56
Renita <ira3624@icloud.com>
2023-09-23 22:30:15 +0300

    Update RecommendedFoodPage.styled.jsx

--- src/pages/RecommendedFoodPage/RecommendedFoodPage.styled.jsx
+++ src/pages/RecommendedFoodPage/RecommendedFoodPage.styled.jsx
@@ -13,11 +13,19 @@ export const Title = styled.h1`
   margin-left: 34px;
 `
 export const Img = styled.img`
-width: 436px;
-height: 460px;
+width: 300px;
+height: 312px;
 margin-top: 24px;
 margin-right: 44px;
-margin-left: 50px
+margin-left: 50px;
+@media (min-width: 320px) and (max-width: 833px)  {
+  width: 380px;
+  height: 396px;
+}
+@media (min-width: 834px) and (max-width: 1439px)  {
+  width: 436px;
+  height: 460px;
+}
 `
 export const Item = styled.li`
 border-radius: 12px;
@@ -25,13 +33,21 @@ border: 1px solid black;
 display: flex;
 justify-content: flex-start;
 align-items: center;
-width: 270px;
+width: 300px;
 height: 86px;
 margin-right: 20px;
 margin-bottom: 20px;
 font-size: 12px;
 background-color:${p => p.theme.colors.black2};
-color:${p => p.theme.colors.white}
+color:${p => p.theme.colors.white};
+
+@media (min-width: 320px) and (max-width: 833px)  {
+  width: 380px;
+}
+@media (min-width: 834px) and (max-width: 1439px)  {
+  width: calc((100% - 60px) / 2);
+  max-width: 328px;
+}
 `
 export const Container = styled.div`
 display: flex;
@@ -50,12 +66,29 @@ color:${p => p.theme.colors.gray}
 `
 
 export const List = styled.ul`
-display: flex;
+
+@media (min-width: 320px) and (max-width: 1003px)  {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top:24px;
+}
+@media (min-width: 834px) and (max-width: 1439px)  {
+  display: flex;
 flex-direction: row;
 flex-wrap: wrap;
 align-items: center;
+}
 `
 export const ContainerTwo = styled.div`
 display: flex;
 justify-content: space-around;
+
+@media (min-width: 320px) and (max-width: 1003px)  {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 `
