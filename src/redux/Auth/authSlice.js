e00f3646f1cfd7026532f5d36d57c5e407b6df7f
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-09-24 08:41:00 +0300

    adds Modal Window Record Diary

--- src/redux/Auth/authSlice.js
+++ src/redux/Auth/authSlice.js
@@ -1,45 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { register, logIn, logOut, currentUser,  } from "./authOperations";
+import { createSlice } from '@reduxjs/toolkit';
+import { register, logIn, logOut, currentUser } from './authOperations';
 
 const initialState = {
-    user: { name: null, email: null },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: true,
+  isRefreshing: false,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    extraReducers: (builder) => {
-        builder
-            .addCase(register.fulfilled, (state, action) => {
-                state.user = action.payload.user;
-                state.token = action.payload.token;
-                state.isLoggedIn = true; 
-            })
-            .addCase(logIn.fulfilled, (state, action) => {
-                state.user = action.payload.user;
-                state.token = action.payload.token;
-                state.isLoggedIn = true;
-            })
-            .addCase(logOut.fulfilled, (state) => {
-                state.user = { name: null, email: null };
-                state.token = null;
-                state.isLoggedIn = false;
-            })
-            .addCase(currentUser.pending, (state) => {
-                state.isRefreshing = true;
-            })
-            .addCase(currentUser.fulfilled, (state, action) => {
-                state.user = action.payload;
-                state.isLoggedIn = true;
-                state.isRefreshing = false;
-            })
-            .addCase(currentUser.rejected, (state) => {
-                state.isRefreshing = false;
-            })
-    },
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(currentUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(currentUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(currentUser.rejected, state => {
+        state.isRefreshing = false;
+      });
+  },
 });
 
 export const authReducer = authSlice.reducer;
