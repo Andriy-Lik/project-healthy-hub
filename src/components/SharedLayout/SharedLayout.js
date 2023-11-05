afb47fd0dab2f1d2cd2be2f80f2d30617ca16ffd
Andriy-Lik <alan321987@gmail.com>
2023-11-05 22:38:55 +0200

    Прибрав лоадер

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header';
-// import Loader from '../Loader';
 
 const SharedLayout = () => {
   return (
