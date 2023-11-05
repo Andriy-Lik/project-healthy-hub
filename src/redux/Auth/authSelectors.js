167f9d6d47272f89199edc2d20cee74efeee674e
Andriy-Lik <alan321987@gmail.com>
2023-11-05 23:09:29 +0200

    Прибрав зайвий код

--- src/redux/Auth/authSelectors.js
+++ src/redux/Auth/authSelectors.js
@@ -1,7 +1,5 @@
 export const selectUser = state => state.auth.user;
 
-export const selectToken = state => state.auth.token;
-
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
 export const selectIsRefreshing = state => state.auth.isRefreshing;
