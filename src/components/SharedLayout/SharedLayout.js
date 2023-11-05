021ec17855eaff1c518eb1810bc9089f88245d38
Andriy-Lik <alan321987@gmail.com>
2023-11-05 21:00:24 +0200

    Update SharedLayout.js

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header';
-import Loader from '../Loader';
+// import Loader from '../Loader';
 
 const SharedLayout = () => {
   return (
     <div>
       <Header />
-      <Suspense fallback={<Loader />}>
+      <Suspense fallback={null}>
         <Outlet />
       </Suspense>
     </div>
