fc7c9d1b50609461b365bac1e525246793df3f62
Olia Rosiak <olia.rosiak@gmail.com>
2023-11-08 23:45:39 +0200

    upd ProfileModal & styles

--- src/components/HeaderModals/ProfileModal.js
+++ src/components/HeaderModals/ProfileModal.js
@@ -1,53 +1,35 @@
-import React from "react";
-import { useEffect } from 'react';
-import { useDispatch } from "react-redux";
-import { createPortal } from 'react-dom';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logOut } from '../../redux/Auth/authOperations';
 
-import { Overlay, ModalWrapper, Modal, Button } from "./ProfileModal.styled";
+import HeaderOverlay from './HeaderOverlay';
+import { ModalWrapper, Modal, Button } from './ProfileModal.styled';
 
 import settingImg from '../../images/icons/setting-2.svg';
 import logOutImg from '../../images/icons/logout.svg';
 
-const modalRoot = document.querySelector('#header-modal-profile')
-
 export default function ProfileModal({ onCloseModal }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const handleLogOut = () => dispatch(logOut());
 
-    const handleLogOut = () => dispatch(logOut());
-
-    useEffect(() => {
-        const escKeyHandler = event => {
-            if (event.code === 'Escape') {
-                onCloseModal();
-            }
-        };
-        window.addEventListener('keydown', escKeyHandler);
-        return () => {
-            window.removeEventListener('keydown', escKeyHandler);
-        };
-    }, [onCloseModal]);
-
-    return createPortal(
-        <Overlay onClick={onCloseModal()}>
-            <ModalWrapper>
-                <Modal onClick={onCloseModal()}>
-                    <Link to={'/settings'}>
-                        <Button type="button">
-                            <img src={settingImg} alt="setting" width={16} />
-                            Setting
-                        </Button>
-                    </Link>
-                    <Link to={'/'}>
-                        <Button type="button" onClick={handleLogOut}>
-                            <img src={logOutImg} alt="log out" width={16} />
-                            Log out
-                        </Button>
-                    </Link>
-                </Modal>
-            </ModalWrapper>
-        </Overlay>,
-        modalRoot
-    )
+  return (
+    <HeaderOverlay onCloseOverlay={onCloseModal}>
+      <ModalWrapper>
+        <Modal onClick={() => onCloseModal()}>
+          <Link to={'/settings'}>
+            <Button type="button">
+              <img src={settingImg} alt="setting" width={16} />
+              Setting
+            </Button>
+          </Link>
+          <Link to={'/'}>
+            <Button type="button" onClick={handleLogOut}>
+              <img src={logOutImg} alt="log out" width={16} />
+              Log out
+            </Button>
+          </Link>
+        </Modal>
+      </ModalWrapper>
+    </HeaderOverlay>
+  );
 }
--- src/components/HeaderModals/ProfileModal.styled.jsx
+++ src/components/HeaderModals/ProfileModal.styled.jsx
@@ -1,49 +1,37 @@
 import styled from 'styled-components';
 
-export const Overlay = styled.div`
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-`;
-
 export const ModalWrapper = styled.div`
-    position: absolute;
-    width: 158px;
-    box-shadow: 0 4px 14px 0 rgba(227, 255, 168, 0.2);
-    background-color: ${div => div.theme.colors.black2};
-    border-radius: ${div => div.theme.radii.normal};
-    padding: 0;
-    @media screen and (${props => props.theme.mq.mobile}) {
-        right: 10px;
-        top: 54px;
-    }
-    @media screen and (${props => props.theme.mq.tablet}) {
-        right: 27px;
-        top: 80px;
-    }
-    @media screen and (${props => props.theme.mq.desktop}) {
-        right: 34px;
-        top: 80px;
-    }
+  position: absolute;
+  width: 158px;
+  box-shadow: 0 4px 14px 0 rgba(227, 255, 168, 0.2);
+  background-color: ${div => div.theme.colors.black2};
+  border-radius: ${div => div.theme.radii.normal};
+  padding: 0;
+  @media screen and (${props => props.theme.mq.mobile}) {
+    right: 10px;
+  }
+  @media screen and (${props => props.theme.mq.tablet}) {
+    right: 27px;
+  }
+  @media screen and (${props => props.theme.mq.desktop}) {
+    right: 34px;
+  }
 `;
 
 export const Modal = styled.div`
-    padding: 24px;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 `;
 
 export const Button = styled.button`
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    color: ${p => p.theme.colors.white};
-    background-color: transparent;
-    border: none;
-    font-weight: ${p => p.theme.fontWeights.medium};
-    font-size: ${p => p.theme.fontSizes.xs};
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: ${p => p.theme.colors.white};
+  background-color: transparent;
+  border: none;
+  font-weight: ${p => p.theme.fontWeights.medium};
+  font-size: ${p => p.theme.fontSizes.xs};
 `;
