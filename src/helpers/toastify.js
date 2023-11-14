47ee639fa87b3079a826f7b2c8af8648827393f0
Olia Rosiak <olia.rosiak@gmail.com>
2023-11-14 23:18:00 +0200

    Create toastify.js

---
+++ src/helpers/toastify.js
@@ -0,0 +1,29 @@
+import { toast } from 'react-toastify';
+
+export default function toastifyMessage(type, message) {
+  if (type === 'warn') {   
+    return toast.warn(message, {
+      position: 'top-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
+  }
+
+  if (type === 'success') {
+    return toast.success(message, {
+      position: 'top-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
+  }
+}
